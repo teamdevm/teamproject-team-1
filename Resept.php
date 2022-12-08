0c1bd7a34a9871958180add9e8fe5fc173dadc23
normalsuperman <53874730+normalsuperman@users.noreply.github.com>
2022-12-08 09:22:34 +0500

    1-27 Создала таблицу для рецептов
    Пока нет функционала, но есть основная верстка

---
+++ Resept.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+	include "database.php";
+	if (isset($_POST['submit'])) { $name_bludo = $_POST['submit'];} 
+	if (empty($name_bludo)) //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+    {
+		exit ("Вы ничего не выбрали! Пожалуйста, отметьте один ингредиент для поиска.");
+    }
+	
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset=UTF-8">
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<title></title>
+</head>
+<header>	
+		<img  style="position:absolute; top:10px; left:10px; width:50px;"src="img/1.png">
+		<div style="position:absolute; top:15px; left:-70px;"class = "title">поедИм</div>
+		<ul class = "menu" >
+		<li ><a style="color: #372637 ;text-decoration: none; " href ="index.php" "#">главная</a></li>
+		<li ><a style=" position:absolute; color: #372637 ;text-decoration: none; margin-top: -30px; margin-left:150px; " href ="Rand.php" "#">рецепт фортуны</a></li>
+		</ul>
+</header>
+<body>
+
+</body>
+</html>
